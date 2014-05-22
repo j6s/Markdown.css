1cc0b6716850839c488125e6fc64c53616ab5f44
Johannes Hertenstein <johannes@nimius.net>
2014-05-22 18:19:46 +0200

    Create Fonts directory in Mac install script solves #1

--- install_osx_mou.sh
+++ install_osx_mou.sh
@@ -4,6 +4,7 @@ cp -v css/*.css ~/Library/Application\ Support/Mou/CSS/;
 echo "CSS installed successfully";
 sleep 1
 echo "installing fonts";
+mkdir "${HOME}/Library/Fonts/${font}";
 for font in less/fonts/**/*.ttf;
-do cp -v "${font}" "~/Library/Fonts/${font}";
+	do cp -v "${font}" "${HOME}/Library/Fonts/${font}";
 done;
